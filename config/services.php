78f2edf2b2cec6d5892f49450332e93f7bb0e0ed
Alexis Brohan <alexis.brohan@imie.fr>
2019-04-29 15:41:54 +0200

    Add twitter socialite

--- config/services.php
+++ config/services.php
@@ -44,4 +44,10 @@ return [
         ],
     ],
 
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => 'http://imie-laravel.org/email-register',
+    ],
+
 ];

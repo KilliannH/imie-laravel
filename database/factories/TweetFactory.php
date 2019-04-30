56698ff70d84270c3597bacaded9a115ed0318b4
KilliannH <killiannhervagault@gmail.com>
2019-04-30 16:06:32 +0200

    added TweetSeeder to generate tweets

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -6,11 +6,10 @@ use App\Tweet;
 use Faker\Generator as Faker;
 
 $factory->define(Tweet::class, function (Faker $faker) {
-    global $isFuture;
     return [
         'content' => $faker->realText(),
-        'publishDate' => $faker->dateTimeBetween($isFuture?"+1 week":"-1 week"),
-        'tweet_id' => $isFuture ? null:$faker->unique()->randomNumber,
+        'publishDate' => $faker->dateTimeBetween("-1 week"),
+        'tweet_id' => $faker->unique()->randomNumber,
         'sent' => false,
         'user_id' => $faker->unique()->randomNumber,
     ];

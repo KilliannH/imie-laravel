7d6f68eaf9663c60b6ab763166bde07d9a11a772
KilliannH <killiannhervagault@gmail.com>
2019-04-30 18:35:58 +0200

    detached TweetService from TweetController

--- app/Console/Commands/Tweetcommand.php
+++ app/Console/Commands/Tweetcommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 use App\Services\TweetService;
+use DateTime;
+use Exception;
 use Illuminate\Console\Command;
 
 class Tweetcommand extends Command
@@ -40,13 +42,31 @@ class Tweetcommand extends Command
      */
     public function handle()
     {
+        // make tweets from json works here
+
         $tweets = $this->tweetService->getTweets();
 
-        foreach ($tweets as $tweet) {
-            dd($tweet);
-            //if (!$tweet->sent) {
+        $now = new DateTime();
 
-            //}
+        $tweetSorted = [];
+
+        foreach ($tweets as $tweet) {
+            if (!$tweet->sent) {
+
+                try {
+                    $date = new DateTime($tweet->publishDate);
+                } catch (Exception $e) {
+                    echo $e->getMessage();
+                    exit(1);
+                }
+
+                if($date < $now) {
+                    array_push($tweetSorted, $tweet);
+                }
+            }
         }
+
+        // all tweets needed to be published :
+        dd(count($tweetSorted));
     }
 }
--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Services\TweetService;
+use App\Tweet;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class TweetController extends Controller
 {
@@ -15,22 +17,63 @@ class TweetController extends Controller
     }
 
     public function getTweets() {
-        return $this->tweetService->getTweets();
+        $tweets = $this->tweetService->getTweets();
+        $response = ['tweets' => $tweets];
+
+        return response()->json($response, 200);
     }
 
     public function getTweet($id) {
-        return $this->tweetService->getTweet($id);
+        $tweet = $this->tweetService->getTweet($id);
+        if(!$tweet) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        $response = ['tweet' => $tweet];
+        return response()->json($response, 200);
     }
 
+    // we do not use the service here
+    // you cannot post if you're not authenticated
     public function  postTweet(Request $request) {
-        return $this->tweetService->postTweet($request);
+        $newTweet = new Tweet();
+        $newTweet->content = $request->input('content');
+        $newTweet->publishDate = $request->input('publishDate');
+        $newTweet->sent = false;
+
+        $provider = 'twitter';
+        $connectedUser = Socialite::driver($provider)->user();
+
+        $user = User::find($connectedUser->id);
+
+        if(!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $newTweet->user()->associate($user);
+        $newTweet->save();
+        return response()->json(['tweet' => $newTweet], 201);
     }
 
+    // neither here
     public function  putTweet(Request $request, $id) {
-        return $this->tweetService->putTweet($request, $id);
+        $editedTweet = Tweet::find($id)->first();
+        if(!$editedTweet) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        $editedTweet->content = $request->input('content');
+        $editedTweet->publishDate = $request->input('publishDate');
+        $editedTweet->sent = $request->input('sent');
+        $editedTweet->tweet_id = $request->input('tweet_id');
+
+        $editedTweet->save();
+        return response()->json(['tweet' => $editedTweet], 200);
     }
 
     public function deleteTweet($id) {
-        return $this->tweetService->deleteTweet($id);
+        $deletedTweet = $this->tweetService->removeTweet($id);
+        if(!$deletedTweet) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        return response()->json(['message' => 'Tweet deleted'], 200);
     }
 }

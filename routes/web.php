c751fe6f35494a94991a6c59dc1e1154daa73ba8
KilliannH <killiannhervagault@gmail.com>
2019-05-02 16:01:42 +0200

    added details view

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,9 @@ Route::get('email-register', 'Auth\LoginController@emailRegister');
 Route::post('email-register-post', 'Auth\LoginController@emailRegisterPost')->name('email-register-post');
 Route::post('post-new-tweet', 'TweetController@postTweet')->name('post-new-tweet');
 // Route::post('post-tweet', 'TweetController@postTweet')->name('post-tweet');
-<<<<<<< HEAD
-Route::get('/home', 'DashboardController@index');
+
 Route::get('/details/{id}', 'TweetDetailsController@index')->name('tweet-details');
-=======
 Route::get('/home', 'DashboardController@index')->name('home');
 
 Route::get('generate-tweets', 'GenerateTweetsController@form')->name('generate-tweets-form');
 Route::post('generate-tweets-post', 'GenerateTweetsController@submit')->name('generate-tweets-post');
->>>>>>> afdd8e2918c675413731242b4005c0d733e6400b

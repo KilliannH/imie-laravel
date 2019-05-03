40439aaa644ba4605e8f470bdfcce978e3803e6a
Alexis Brohan <alexis.brohan@imie.fr>
2019-05-03 10:00:57 +0200

    Add auth for tweets generating controller

--- app/Http/Controllers/GenerateTweetsController.php
+++ app/Http/Controllers/GenerateTweetsController.php
@@ -10,6 +10,11 @@ use Illuminate\Http\Request;
 class GenerateTweetsController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function form () {
         return view('generate-tweets-form');
     }

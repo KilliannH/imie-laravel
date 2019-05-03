e442903cdcc9444adb0da4ef7f5135462129c1f3
KilliannH <killiannhervagault@gmail.com>
2019-05-03 11:14:55 +0200

    added thomas's tweeter credentials

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -14,8 +14,8 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
+	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', 'tw3eJw56bQIkuQgdXqMuyVFMP') : '',
+	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', 'UtAimzxIkRFNPVx4HEDH83jAHjvWYIv1WZHYceX9RTTMJVhsem') : '',
+	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '497364333-GfcHJ9S1eCJdYPFEflMIJH09ALzKN8V1IrWeaO2Z') : '',
+	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', 'spaX7kqVef4BSCw0ebMKIzZMy2aSjye2ljjVZOnBRDhTP') : '',
 ];
